8acec00a932837044ff8f41d069b73589b6c4ee9
Balasundar <balasundar2796@gmail.com>
2021-05-22 08:54:44 +0530

    update main file

--- main.py
+++ main.py
@@ -5,16 +5,6 @@ from pydantic import BaseModel
 import uvicorn
 import api
 
-
-class CarModel(BaseModel):
-    manufacturer: str
-    modelName: str
-    cc: int
-    onRoadPrice: int
-    seatingCapacity: int
-    gearBox: int
-
-
 # Initialize the app
 app = FastAPI()
 
@@ -25,7 +15,3 @@ app.include_router(api.router)
 @app.get('/')
 def root_api():
     return {"message": "Welcome to Balasundar's Technical Blog"}
-
-
-if __name__ == "main":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
